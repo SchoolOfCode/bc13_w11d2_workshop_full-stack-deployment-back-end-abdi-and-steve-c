52d5ebcec04986460d26de85b44c6bc8b647e45f
Sunbearian <stigcollins@icloud.com>
2022-12-06 16:31:46 +0000

    updated query

--- models/shoppingList.js
+++ models/shoppingList.js
@@ -1,25 +1,28 @@
 import { pool } from "../db/index.js";
 
 export async function getShoppingList() {
-  const data = await pool.query("SELECT * FROM shopping;");
-  console.log("The shopping list is", data.rows);
-  return data.rows;
+	const data = await pool.query("SELECT * FROM shopping ORDER BY id;");
+	console.log("The shopping list is", data.rows);
+	return data.rows;
 }
 
 export async function postListItem(listItem) {
-  const { item, completed } = listItem;
-  const data = await pool.query(
-    `INSERT INTO shopping (
+	const { item, completed } = listItem;
+	const data = await pool.query(
+		`INSERT INTO shopping (
       item,
       completed
     ) VALUES ($1,$2) RETURNING *;`,
-    [item, completed]
-  );  
-  return data.rows[0];
+		[item, completed]
+	);
+	return data.rows[0];
 }
 export async function updateListById(id, updates) {
-  // Query the database to update an author and return the newly updated author
-  const result = await pool.query("UPDATE shopping SET completed= $1 WHERE id = $2 RETURNING *", [updates.completed,id]);
-  const updateListByID = result.rows;
-  return updateListByID;
-};
+	// Query the database to update an author and return the newly updated author
+	const result = await pool.query(
+		"UPDATE shopping SET completed= $1 WHERE id = $2 RETURNING *",
+		[updates.completed, id]
+	);
+	const updateListByID = result.rows;
+	return updateListByID;
+}
